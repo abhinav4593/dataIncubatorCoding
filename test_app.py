b0dc03da5d8c2c77b4815c5ab62bdd428c3ec0b3
Abhinav Gundlapalli <31975831+abhinav4593@users.noreply.github.com>
2018-04-03 02:25:08 -0500

    Reorganised the structure to handle tha path build issue

--- test_app.py
+++ test_app.py
@@ -25,7 +25,7 @@ class FlaskTestCase(TestCase):
 
     def test_post(self):
         data={}
-        with open("/imgTest/houston-cougars.jpg",'r') as img1:
+        with open("houston-cougars.jpg",'r') as img1:
             imgStringIO1 = StringIO(img1.read())
         data['file'] = (imgStringIO1, 'houston-cougars.jpg')
         response = self.client.post(url_for('upload_file'), data=data, follow_redirects=True, content_type='multipart/form-data')
